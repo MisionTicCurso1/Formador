2540cd34ab4d1732155353853fc3058043cd991b
CristianPalta1 <cristian.palta.c@uniautonoma.edu.co>
2021-08-10 19:24:34 -0500

    Codigo clase 6 10/08/2021

---
+++ Codigo/codigo2.py
@@ -0,0 +1,63 @@
+
+# import math
+
+# print(math.pi)
+# print(math.e)
+# print(math.tau)
+
+# numero = 5
+# numero = numero ** 2
+
+# print("Elevado a la potencia", numero)
+
+
+# print("Con la funcion pow", math.pow(5, 2))
+
+
+
+# numero2 = 25 
+# resultado = math.pow(numero2, 0.5)
+
+# print("Raiz 1", resultado)
+
+
+# resultado2 = math.sqrt(numero2)
+
+# print("Raiz con math sqrt", resultado2)
+
+
+## ctrl + k + c para comentar
+## ctrl + k + u para descomentar
+
+variable = 20
+año =  2021
+
+#Funcion que no reciba parametros y no retorne nada
+def updateData():
+    print("Hola entraste a ver una suma")
+    variable1 = 15
+    variable2 = 16
+    res = 0
+    res = variable1 +  variable2 + variable
+    print(f"Esta es la suma, {res}")
+    print("Esta es la suma", res)
+
+updateData()
+
+#Funciones con parametro
+def knowMyAge(añoNacimiento):
+    resEdad = año - añoNacimiento
+    print("Tu edad es:", resEdad)
+
+knowMyAge(1994)
+
+
+#Funcion con parametro y retorna dato
+def calculate(pesos):
+
+    h = 'hola'
+    return (h) 
+    
+precioDolar = calculate(5000)
+# precio = kjhdslkfjahsdf(45)
+
--- Codigo/main.py
+++ Codigo/main.py
@@ -1,61 +1,92 @@
-numero1 = 15
-numero2 = 16
-resultado = 0
+# numero1 = 15
+# numero2 = 16
+# resultado = 0
 
-resultado = numero1 + numero2
-
-
-print("Vea el resultado es el siguiente: ", resultado)
+# resultado = numero1 + numero2
 
 
-
-numero3 = 60
-
-# % es para guardar en memoria el resultado
-# / es para guardar en memoria el cociente
-
-if(numero3 % 2 == 0):
-    print("Ese numero es par")
-else:
-    print("Ese numero es impar")
-
-
-if numero3 != 40:
-    print("SI es diferente. El numero comparado es: ", numero3)
-else:
-    print("Son los mismos numeros")
-
-if (numero3 > 60):
-    print("Si es mayor a 60 ")
-else:
-    print("No es mayor que 60")
-
-if (numero3 >= 60):
-    print("Si es mayor e igual 60 ")
-else:
-    print("No es mayor que 60")
+# print("Vea el resultado es el siguiente: ", resultado)
 
 
 
+# numero3 = 60
 
-# La discoteca solo acepta jovenes que esten vestidos de negro y tengan 18años
+# # % es para guardar en memoria el resultado
+# # / es para guardar en memoria el cociente
 
-ropa = 'verde'
-edad = 18
-
-if(ropa == 'negro' and edad == 18):
-    print("Cumple con las reglas de la discoteca")
-else:
-    print("No cumple con las reglas de la discoteca")
-
-
-if(ropa == 'negro' or edad == 18):
-    print("Cumple con las reglas de la discoteca ..")
-else:
-    print("No cumple con las reglas de la discoteca ..")
+# if(numero3 % 2 == 0):
+#     print("Ese numero es par")
+# else:
+#     print("Ese numero es impar")
 
 
-#Queremos recibir datos por consola
-print("Hola mucho como te llamas ?")
-nombre = input()
-print("Mucho en conocerte", nombre)
+# if numero3 != 40:
+#     print("SI es diferente. El numero comparado es: ", numero3)
+# else:
+#     print("Son los mismos numeros")
+
+# if (numero3 > 60):
+#     print("Si es mayor a 60 ")
+# else:
+#     print("No es mayor que 60")
+
+# if (numero3 >= 60):
+#     print("Si es mayor e igual 60 ")
+# else:
+#     print("No es mayor que 60")
+
+
+
+
+# # La discoteca solo acepta jovenes que esten vestidos de negro y tengan 18años
+
+# ropa = 'verde'
+# edad = 18
+
+# if(ropa == 'negro' and edad == 18):
+#     print("Cumple con las reglas de la discoteca")
+# else:
+#     print("No cumple con las reglas de la discoteca")
+
+
+# if(ropa == 'negro' or edad == 18):
+#     print("Cumple con las reglas de la discoteca ..")
+# else:
+#     print("No cumple con las reglas de la discoteca ..")
+
+
+# #Queremos recibir datos por consola
+
+# ingreso_Teclado = int (input())
+
+# potencia = ingreso_Teclado ** 2
+
+
+## ctrl + k + c comentar
+## ctrl + k + u descomentar
+
+# print("ASi se elevo a la potencia", potencia )
+
+sensor = 8
+print("Sensor 1.0", sensor)
+
+sensor = sensor - 3
+
+print("Sensor 1", sensor)
+
+
+sensor2 = 8
+print("Sensor 2.0", sensor2)
+
+sensor2 -= 3
+
+print("Sensor 2", sensor2)
+
+
+
+
+
+
+
+
+
